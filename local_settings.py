3bcf498cf2d9fc90931074290ebe683262219538
ceceiliaallwein <ceceilia.allwein@gmail.com>
2013-12-05 17:39:52 -0500

    QuoteBot commits

--- local_settings.py
+++ local_settings.py
@@ -5,10 +5,10 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
 # Uncommet to configure in file.
-ACCOUNT_SID = "AC802d88dd13f2e3f8d8bc3f9b95eb9f06" 
-AUTH_TOKEN = "a610e5b81d025523706a2bbc381bf5ac"
-BSSSPAM_APP_SID = "AP69a2d4c9cdceee480dfa9e9230c586c5"
-BSS_SPAM_ID = "+17032913341"
+ACCOUNT_SID = "ACfe77d42a27d74a5e199e9a6dda6dd842" 
+AUTH_TOKEN = "dd51a79f15911993c807c9acfec56805"
+BSSSPAM_APP_SID = "AP55176de141377159202b0ce15c97f185"
+BSS_SPAM_ID = "+12602714996"
 
 
 # Begin Heroku configuration - configured through environment variables.
